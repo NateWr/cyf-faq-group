50e5cff3f21b3c8ff3f88c7772b6f9b1a4cf82ff
YJohn <yfekadu2016@gmail.com>
2017-09-14 19:53:04 +0100

    #6 creating client side functionality, listen all the buttons ...
    sending AJAX request to the route called 'rating' then resiving the change and display all the changes

--- public/main.js
+++ public/main.js
@@ -1,17 +1,35 @@
-var helpful = 0;
-var unhelpful = 0;
-var total = 0;
-var display = document.getElementById("displayCount");
-var totalNumber = document.getElementById("totalNumber")
-document.getElementById("Button1").addEventListener("click", function (event) {
-    //var temp = event.target;
-    helpful++;
-    total++;
-    display.innerHTML = helpful;
-    totalNumber.innerHTML = total;
-});
-document.getElementById("Button2").addEventListener("click", function (event) {
-    unhelpful++;
-    total++;
-    totalNumber.innerHTML = total;
-});
+const clickEvent = function (event) {
+    const id = event.target.getAttribute('data-question-id');
+    const helpful = event.target.getAttribute('data-helpful');
+
+    const options = {
+        method: "POST",
+        body: {
+            id: id,
+            helpful: helpful
+        }
+    };
+    fetch("/rating", options)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            console.log(json);
+            receiveResponse(json);
+        })
+    //  $ajax(options).done(receiveResponse);
+}
+const receiveResponse = function (res) {
+    const valueEls = document.querySelectorAll('.value-helpful');
+    for (let valueEl of valueEls) {
+        let valueElId = valueEl.getAttribute('data-question-id')
+        if (res._id === valueElId) {
+            let totalNumber = res.helpful + res.notHelpful;
+            valueEl.innerHTML = res.helpful + '/' + totalNumber + 'people found this helpful. was this entry helpful';
+        }
+    }
+}
+const buttons = document.querySelectorAll('.button-feedback');
+for (let button of buttons) {
+    button.addEventListener('click', clickEvent);
+}
